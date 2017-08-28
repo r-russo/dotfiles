9a5d9519ca413ba2ccbebb3ecce13ef7db1172ae
Rodrigo Russo <rodrigo.e.russo@gmail.com>
2017-08-28 10:27:34 -0300

    tmux

--- zprofile
+++ zprofile
@@ -1,3 +1,2 @@
 typeset -U path
 path=(~/bin $path[@])
-
--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -8,3 +8,8 @@ alias pacsa='pacaur -Ssa'
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
+alias ta='todo.sh add'
+alias tl='todo.sh list'
+alias t='todo.sh'
+
+alias emg='cd ~/PPS/emg'
