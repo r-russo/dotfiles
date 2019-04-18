7705129a9bfd8c462ac1207d9361bf05c6a8225e
Rodrigo Russo <rodrigo.e.russo@gmail.com>
2019-04-18 11:11:57 -0300

    laptop

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -3,14 +3,13 @@
 setxkbmap latam &
 
 if [ "$(hostname)" = "thinkpad" ]; then
-    sxhkd -m 1 -c ~/.config/sxhkd/sxhkdrc-laptop &
     polybar main_laptop &
 else
-    sxhkd -m 1 &
     polybar main &
     # firefox &
     redshift &
 fi
+sxhkd -m 1 &
 numlockx on &
 xsetroot -cursor_name left_ptr &
 nitrogen --restore &
